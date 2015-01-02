54886caea967b6d57b9c3effb704695707905788
Tatsuya Maki <t28oogle@gmail.com>
2015-01-02 15:48:35 +0900

    WeatherParserを追加

---
+++ app/src/main/java/com/t28/rxweather/WeatherParser.java
@@ -0,0 +1,34 @@
+package com.t28.rxweather;
+
+import android.support.annotation.NonNull;
+
+import com.android.volley.VolleyError;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import java.io.IOException;
+
+public class WeatherParser {
+    private final ObjectMapper mMapper;
+
+    public WeatherParser() {
+        this(new ObjectMapper());
+    }
+
+    WeatherParser(@NonNull ObjectMapper mapper) {
+        mMapper = mapper;
+    }
+
+    public Weather parse(byte[] body) throws ParseException {
+        try {
+            mMapper.readValue(body, Weather.class);
+        } catch (IOException e) {
+            throw new ParseException(e);
+        }
+    }
+
+    public static class ParseException extends VolleyError {
+        public ParseException(Throwable cause) {
+            super(cause);
+        }
+    }
+}
