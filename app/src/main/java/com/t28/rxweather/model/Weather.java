6a2b79eea915c2c99edd3d33f877317a7d1c8d4c
Tatsuya Maki <t28oogle@gmail.com>
2015-01-02 18:05:43 +0900

    WeatherのValidatable実装

--- app/src/main/java/com/t28/rxweather/model/Weather.java
+++ app/src/main/java/com/t28/rxweather/model/Weather.java
@@ -1,12 +1,17 @@
 package com.t28.rxweather.model;
 
+import android.text.TextUtils;
+
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import com.fasterxml.jackson.databind.annotation.JsonPOJOBuilder;
+import com.t28.rxweather.Validatable;
 
 @JsonDeserialize(builder = Weather.Builder.class)
-public class Weather {
+public class Weather implements Validatable {
+    private static final int NO_CITY_ID = -1;
+
     private final int mCityId;
     private final String mCityName;
     private final String mCountryCode;
@@ -27,6 +32,30 @@ public class Weather {
 
         mCoordinate = builder.mCoordinate;
         mAttribute = builder.mAttribute;
+    }
+
+    @Override
+    public boolean isValid() {
+        if (mCityId == NO_CITY_ID) {
+            return false;
+        }
+
+        if (TextUtils.isEmpty(mCityName) || TextUtils.isEmpty(mCountryCode)) {
+            return false;
+        }
+
+        if (mSunriseTime <= 0 || mSunsetTime <= 0) {
+            return false;
+        }
+
+        if (mCoordinate == null || !mCoordinate.isValid()) {
+            return false;
+        }
+
+        if (mAttribute == null || !mAttribute.isValid()) {
+            return false;
+        }
+        return true;
     }
 
     public int getCityId() {
@@ -60,7 +89,7 @@ public class Weather {
     @JsonPOJOBuilder(withPrefix = "set")
     @JsonIgnoreProperties(ignoreUnknown = true)
     public static class Builder {
-        private int mCityId;
+        private int mCityId = NO_CITY_ID;
         private String mCityName;
         private String mCountryCode;
 
