579b68d994cc1819f2a352a3b08da4c8ea430f76
Tatsuya Maki <t28oogle@gmail.com>
2015-01-02 15:57:07 +0900

    Weatherにパラメータ追加

--- app/src/main/java/com/t28/rxweather/Weather.java
+++ app/src/main/java/com/t28/rxweather/Weather.java
@@ -1,4 +1,19 @@
 package com.t28.rxweather;
 
 public class Weather {
+    private int mCityId;
+    private String mCityName;
+    private String mCountryCode;
+
+    private double mLat;
+    private double mLon;
+
+    private long mSunriseTime;
+    private long mSunsetTime;
+
+    private double mTemperature;
+    private double mMinTemperature;
+    private double mMaxTemperature;
+
+    private int mHumidity;
 }
