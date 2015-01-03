e84d506a218dc59cf402ed8d3d1d38e76f772bbb
Tatsuya Maki <t28oogle@gmail.com>
2015-01-03 15:16:53 +0900

    OpenWeatherMap用のフィールドを追加

---
+++ app/src/main/java/com/t28/rxweather/parser/OpenWeatherMapField.java
@@ -0,0 +1,25 @@
+package com.t28.rxweather.parser;
+
+public class OpenWeatherMapField {
+    public static final String CITY = "city";
+    public static final String CITY_ID = "id";
+    public static final String CITY_NAME = "name";
+    public static final String COUNTRY_CODE = "code";
+
+    public static final String COORDINATE = "coord";
+    public static final String LATITUDE = "lat";
+    public static final String LONGITUDE = "lon";
+
+    public static final String RECEIVED_TIME = "dt";
+    public static final String MAIN_ATTRIBUTE = "main";
+
+    public static final String TEMPERATURE = "temp";
+    public static final String MIN_TEMPERATURE = "temp_min";
+    public static final String MAX_TEMPERATURE = "temp_max";
+
+    public static final String HUMIDITY = "humidity";
+    public static final String PRESSURE = "pressure";
+
+    private OpenWeatherMapField() {
+    }
+}
