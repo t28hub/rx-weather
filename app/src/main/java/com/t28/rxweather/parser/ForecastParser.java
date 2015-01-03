1666b24f5ef1e9cc5011ce10db2325c961fe9b6a
Tatsuya Maki <t28oogle@gmail.com>
2015-01-03 14:28:23 +0900

    ForecastParserの追加

---
+++ app/src/main/java/com/t28/rxweather/parser/ForecastParser.java
@@ -0,0 +1,18 @@
+package com.t28.rxweather.parser;
+
+public class ForecastParser extends JsonParser {
+    private static final String PROPERTY_CITY = "city";
+    private static final String PROPERTY_CITY_ID = "id";
+    private static final String PROPERTY_CITY_NAME = "name";
+    private static final String PROPERTY_COUNTRY_CODE = "code";
+    private static final String PROPERTY_COORDINATE = "coord";
+    private static final String PROPERTY_LATITUDE = "lat";
+    private static final String PROPERTY_LONGITUDE = "lon";
+    private static final String PROPERTY_MAIN_ATTRIBUTE = "main";
+    private static final String PROPERTY_RECEIVED_TIME = "dt";
+    private static final String PROPERTY_TEMPERATURE = "temp";
+    private static final String PROPERTY_MIN_TEMPERATURE = "temp_min";
+    private static final String PROPERTY_MAX_TEMPERATURE = "temp_max";
+    private static final String PROPERTY_HUMIDITY = "humidity";
+    private static final String PROPERTY_PRESSURE = "pressure";
+}
