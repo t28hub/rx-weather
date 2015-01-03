24e83f5518aa1602059e67789cd9791aed5885fa
Tatsuya Maki <t28oogle@gmail.com>
2015-01-03 23:17:47 +0900

    モデル用の抽象クラスを追加

---
+++ app/src/main/java/com/t28/rxweather/model/Model.java
@@ -0,0 +1,25 @@
+package com.t28.rxweather.model;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+public abstract class Model {
+
+    @Override
+    public String toString() {
+        final StringBuilder builder = new StringBuilder();
+        builder.append(this.getClass().getCanonicalName());
+
+        final ObjectMapper mapper = new ObjectMapper();
+        try {
+            builder.append(mapper.writeValueAsString(this));
+            return builder.toString();
+        } catch (JsonProcessingException e) {
+            return super.toString();
+        }
+    }
+
+    public abstract boolean isValid();
+
+    public abstract boolean isEmpty();
+}
