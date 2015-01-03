e3ff9f2c35a35ddc23ffe743a425e2db0a7bbeb6
Tatsuya Maki <t28oogle@gmail.com>
2015-01-04 00:43:32 +0900

    不要なインポートの削除

--- app/src/main/java/com/t28/rxweather/model/Weather.java
+++ app/src/main/java/com/t28/rxweather/model/Weather.java
@@ -1,12 +1,8 @@
 package com.t28.rxweather.model;
 
-import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import com.fasterxml.jackson.annotation.PropertyAccessor;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import com.fasterxml.jackson.databind.annotation.JsonPOJOBuilder;
 import com.t28.rxweather.request.WeatherRequest;
