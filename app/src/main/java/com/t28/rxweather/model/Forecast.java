640cb6b4e409a23489de7d04f3a1c713ba5d3211
Tatsuya Maki <t28oogle@gmail.com>
2015-01-03 16:08:22 +0900

    Forecast.Builderの実装を追加

--- app/src/main/java/com/t28/rxweather/model/Forecast.java
+++ app/src/main/java/com/t28/rxweather/model/Forecast.java
@@ -1,8 +1,48 @@
 package com.t28.rxweather.model;
 
+import com.t28.rxweather.util.CollectionUtils;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class Forecast implements Validatable {
+    private final City mCity;
+    private final List<Weather> mWeathers;
+
+    private Forecast(Builder builder) {
+        mCity = builder.mCity;
+        if (CollectionUtils.isEmpty(builder.mWeathers)) {
+            mWeathers = Collections.emptyList();
+        } else {
+            mWeathers = new ArrayList<>(builder.mWeathers);
+        }
+    }
+
     @Override
     public boolean isValid() {
         return false;
+    }
+
+    public static class Builder {
+        private City mCity;
+        private List<Weather> mWeathers;
+
+        public Builder() {
+        }
+
+        public Builder setCity(City city) {
+            mCity = city;
+            return this;
+        }
+
+        public Builder setWeathers(List<Weather> weathers) {
+            mWeathers = weathers;
+            return this;
+        }
+
+        public Forecast build() {
+            return new Forecast(this);
+        }
     }
 }
--- app/src/main/java/com/t28/rxweather/util/CollectionUtils.java
+++ app/src/main/java/com/t28/rxweather/util/CollectionUtils.java
@@ -1,9 +1,17 @@
 package com.t28.rxweather.util;
 
+import java.util.List;
 import java.util.Map;
 
 public class CollectionUtils {
     private CollectionUtils() {
+    }
+
+    public static boolean isEmpty(List list) {
+        if (list == null) {
+            return true;
+        }
+        return list.isEmpty();
     }
 
     public static <T1, T2> T2 getValue(Map<T1, T2> map, T1 key, T2 defaultValue) {
