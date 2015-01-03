9883199c5a56dacec418c60a54443b220c95a111
Tatsuya Maki <t28oogle@gmail.com>
2015-01-03 16:30:16 +0900

    ForecastParserの実装追加

--- app/src/main/java/com/t28/rxweather/parser/ForecastParser.java
+++ app/src/main/java/com/t28/rxweather/parser/ForecastParser.java
@@ -1,4 +1,16 @@
 package com.t28.rxweather.parser;
 
-public class ForecastParser {
+import com.t28.rxweather.model.Forecast;
+
+import java.io.IOException;
+
+public class ForecastParser extends JacksonParser<Forecast> {
+    @Override
+    public Forecast parse(byte[] body) throws ParseException {
+        try {
+            return getMapper().readValue(body, Forecast.class);
+        } catch (IOException e) {
+            throw new ParseException(e);
+        }
+    }
 }
