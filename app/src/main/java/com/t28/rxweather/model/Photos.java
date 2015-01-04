48e2f35cc5303282b49ba942b33673c148104e6c
Tatsuya Maki <t28oogle@gmail.com>
2015-01-04 16:42:37 +0900

    PhotosにPhoto一覧を追加

--- app/src/main/java/com/t28/rxweather/model/Photos.java
+++ app/src/main/java/com/t28/rxweather/model/Photos.java
@@ -1,16 +1,28 @@
 package com.t28.rxweather.model;
 
+import com.t28.rxweather.util.CollectionUtils;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class Photos extends Model {
     private final int mPage;
     private final int mPageCount;
     private final int mPerPage;
     private final int mTotal;
+    private final List<Photo> mPhotos;
 
     private Photos(Builder builder) {
         mPage = builder.mPage;
         mPageCount = builder.mPageCount;
         mPerPage = builder.mPerPage;
         mTotal = builder.mTotal;
+        if (CollectionUtils.isEmpty(builder.mPhotos)) {
+            mPhotos = Collections.emptyList();
+        } else {
+            mPhotos = new ArrayList<>(builder.mPhotos);
+        }
     }
 
     @Override
@@ -49,11 +61,20 @@ public class Photos extends Model {
         return mTotal;
     }
 
+    public int count() {
+        return mPhotos.size();
+    }
+
+    public List<Photo> getPhotos() {
+        return new ArrayList<>(mPhotos);
+    }
+
     public static class Builder {
         private int mPage;
         private int mPageCount;
         private int mPerPage;
         private int mTotal;
+        private List<Photo> mPhotos;
 
         public Builder() {
         }
@@ -75,6 +96,11 @@ public class Photos extends Model {
 
         public Builder setTotal(int total) {
             mTotal = total;
+            return this;
+        }
+
+        public Builder setPhotos(List<Photo> photos) {
+            mPhotos = photos;
             return this;
         }
 
