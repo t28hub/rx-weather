4e9b41ce4ba0ee03bd69df168a24a22871fcece7
Tatsuya Maki <t28oogle@gmail.com>
2015-01-04 16:29:53 +0900

    PhotoSizeの追加

---
+++ app/src/main/java/com/t28/rxweather/model/PhotoSize.java
@@ -0,0 +1,40 @@
+package com.t28.rxweather.model;
+
+public enum PhotoSize {
+    SMALL {
+        private static final String SUFFIX = "m";
+
+        @Override
+        public String toSuffix() {
+            return SUFFIX;
+        }
+    },
+    MEDIUM {
+        private static final String SUFFIX = "z";
+
+        @Override
+        public String toSuffix() {
+            return SUFFIX;
+        }
+    },
+    LARGE {
+        private static final String SUFFIX = "b";
+
+        @Override
+        public String toSuffix() {
+            return SUFFIX;
+        }
+    },
+    THUMBNAIL {
+        private static final String SUFFIX = "t";
+
+        @Override
+        public String toSuffix() {
+            return SUFFIX;
+        }
+    };
+
+    public String toSuffix() {
+        throw new AbstractMethodError();
+    }
+}
