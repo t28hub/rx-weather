51dd23d0089bf6e717dc55df1fb996f4449a0bed
Tatsuya Maki <t28oogle@gmail.com>
2015-01-05 00:21:04 +0900

    WeatherServiceの追加

---
+++ app/src/main/java/com/t28/rxweather/data/service/WeatherService.java
@@ -0,0 +1,25 @@
+package com.t28.rxweather.data.service;
+
+import com.android.volley.RequestQueue;
+import com.t28.rxweather.data.model.Coordinate;
+import com.t28.rxweather.data.model.Weather;
+import com.t28.rxweather.request.WeatherRequest;
+import com.t28.rxweather.volley.RxSupport;
+
+import rx.Observable;
+
+public class WeatherService {
+    private final RequestQueue mQueue;
+
+    public WeatherService(RequestQueue queue) {
+        mQueue = queue;
+    }
+
+    public Observable<Weather> findWeather(Coordinate coordinate) {
+        final WeatherRequest request = new WeatherRequest.Builder("")
+                .setLat(coordinate.getLat())
+                .setLon(coordinate.getLon())
+                .build();
+        return new RxSupport(mQueue).createObservableRequest(request);
+    }
+}
