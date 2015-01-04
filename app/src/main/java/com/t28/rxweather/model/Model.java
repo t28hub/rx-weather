f884205754b7eaff0934696feb3e2cd4b95b50e6
Tatsuya Maki <t28oogle@gmail.com>
2015-01-04 20:26:02 +0900

    Modelをinterfaceに変更

--- app/src/main/java/com/t28/rxweather/model/Model.java
+++ app/src/main/java/com/t28/rxweather/model/Model.java
@@ -1,23 +1,5 @@
 package com.t28.rxweather.model;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-
-public abstract class Model {
-
-    @Override
-    public String toString() {
-        final StringBuilder builder = new StringBuilder();
-        builder.append(this.getClass().getCanonicalName());
-
-        final ObjectMapper mapper = new ObjectMapper();
-        try {
-            builder.append(mapper.writeValueAsString(this));
-            return builder.toString();
-        } catch (JsonProcessingException e) {
-            return super.toString();
-        }
-    }
-
-    public abstract boolean isValid();
+public interface Model {
+    boolean isValid();
 }
