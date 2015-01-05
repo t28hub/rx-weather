a80ba3004b9039e3468cb7db211fc282fe1e2f57
Tatsuya Maki <t28oogle@gmail.com>
2015-01-05 20:43:46 +0900

    WeatherViewの追加

---
+++ app/src/main/java/com/t28/rxweather/view/WeatherView.java
@@ -0,0 +1,19 @@
+package com.t28.rxweather.view;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.LinearLayout;
+
+public class WeatherView extends LinearLayout {
+    public WeatherView(Context context) {
+        super(context);
+    }
+
+    public WeatherView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public WeatherView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+}
